64cc4490b0f05430dd72b0ac43beaf0ff096a121
set0xc3 <set0xc3@gmail.com>
2022-12-23 19:33:22 +0300

    backup

--- tools/build-linux.sh
+++ tools/build-linux.sh
@@ -9,11 +9,17 @@ NAME=
 PROJECT_DIR=$ROOT/code/custom
 BUILD_DIR=$ROOT/build
 
+if [ ! -d "$BUILD_DIR" ]; then
+
+mkdir "$BUILD_DIR"
+
+fi
+
 pushd $ROOT/code
-  sh ./bin/build-linux.sh
+  ./bin/build-linux.sh
 popd
 
 pushd $PROJECT_DIR
-  sh ./bin/buildsuper_x64-linux.sh ./ncoder_layer.cpp debug
+  ./bin/buildsuper_x64-linux.sh ./ncoder_layer.cpp debug
   mv -f ./custom_4coder.so $BUILD_DIR/custom_4coder.so
 popd
